389ed956e22a088733d030835a2d61d02a457b3a
Rutuja <patil.rut@northeastern.edu>
2024-10-10 02:23:26 -0400

    setting up context API

--- expense-tracker-react/src/components/AddTransaction.js
+++ expense-tracker-react/src/components/AddTransaction.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AddTransaction = () =>{
+
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0)
+
     return(
         <div>
             <h3>
@@ -9,14 +13,14 @@ export const AddTransaction = () =>{
             <form id="form">
                 <div>
                      <label htmlFor="text">Text</label>
-                     <input type="text" id="text" placeholder='Enter Text....'></input>
+                     <input type="text" value={text} onChange={(e)=> setText(e.target.value)} id="text" placeholder='Enter Text....'></input>
                 </div>
                 <div className='form-control'>
                     <label htmlFor="amount">
                         Amount <br/>
                         (negative - expense, positive - income)
                     </label>
-                    <input type="number" id="amount" placeholder='Enter Amount...'/>
+                    <input type="number" value={amount} onChange={(e)=>{setAmount(e.target.value)}} id="amount" placeholder='Enter Amount...'/>
                 </div>
                 <button className='btn'>
                     Add Transaction
