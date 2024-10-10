7ef00c4e9aa0c24f450ab0fbe9f58d4a5f66368a
Rutuja <patil.rut@northeastern.edu>
2024-10-10 02:48:42 -0400

    implementing global context in balanace component

--- expense-tracker-react/src/components/TransactionList.js
+++ expense-tracker-react/src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState'  
+import Transaction from './Transaction';
 
 export const TransactionList = ()=>{
     const {transactions} = useContext(GlobalContext);
@@ -10,10 +11,8 @@ export const TransactionList = ()=>{
         <div>
             <h3>History</h3>
             <ul id="list" className='list'>
-                <li className='minus'>
-                    Cash <span>-$400</span>
-                    <button className='delete-btn'>x</button>
-                </li>
+                {transactions.map(t => (<Transaction key={t.id} transaction={t}/>))}
+                
             </ul>
         </div>
     )
